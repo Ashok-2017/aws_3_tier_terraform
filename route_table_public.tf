24c6ddcf11c0b9ffe6fee9ef43296c01c8c6e84f
Ashok-2017 <84613826+Ashok-2017@users.noreply.github.com>
2024-01-15 20:55:34 +0800

    Update route_table_public.tf

--- route_table_public.tf
+++ route_table_public.tf
@@ -3,4 +3,21 @@ resource "aws_route_table" "route" {
   vpc_id = "${aws_vpc.astrovpc.id}"
   route {
      cidr_block = "0.0.0.0/0"
-     gateway_id = "${aws_internet_gateway.
+     gateway_id = "${aws_internet_gateway.astrogateway.id}"
+ }
+
+ tags = {
+   Name = "Route to internet" 
+}
+
+#Associating route table
+resource "aws_route_table_association" "astro1" {
+  subnet_id = "${aws_subnet.astrosubnet.id}"
+  route_table_id = "${aws_route_table.id}"
+}
+
+resource "aws_route_table_association" "astro2" {
+  subnet_id = "${aws_subnet.astrosubnet.id}"
+  route_table_id = "${aws_route_table.id}"
+}
+
