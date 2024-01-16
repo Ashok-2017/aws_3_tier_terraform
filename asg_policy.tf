3df4605b52a9cedd379e2f221986d0e906987c95
Ashok-2017 <84613826+Ashok-2017@users.noreply.github.com>
2024-01-16 20:46:21 +0800

    Create asg_policy.tf

---
+++ asg_policy.tf
@@ -0,0 +1,36 @@
+resource "aws_autoscaling_policy" "astro_policy_up" {
+  name = "astro_policy_up"
+  scaling_adjustment = 1
+  adjustment_type = "ChangeInCapacity"
+  cooldown = 300
+  autoscaling_group_name = "${aws_autoscaling_group.web.name}"
+}
+
+resource "aws_cloudwatch_metric_alarm" "web_cpu_alarm_up" {
+  alaram_name = "web_cpu_alarm_up" 
+  comparision_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods = 2
+  metric_name = "CPUUtilization"
+  namespace = "AWS/EC2"
+  period = "60"
+  statistic = "Average"
+  threshold = "80"
+
+  dimensions = {
+   AutoScalingGroupName = "${aws_autoscaling_group_name.web.name}"
+} 
+
+alarm_description = "This metric monitor EC2 CPU instance utilization"
+alarm_actions = ["${aws_autosacling_policy.astro_policy_up.arn}"]
+
+resource "aws_autoscaling_policy" "astro_policy_down" {
+   name = "astro_policy_down"
+   scaling_adjustment = "-1"
+   adjustment_type = "ChangeInCapacity"
+   cooldown = "300"
+   autoscaling_group_name = "${aws_autoscaling_group.astro_policy_down.arn}"
+}
+
+
+
+
